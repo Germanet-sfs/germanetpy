33af173326dae0399fa9d8369c58da2ceab5a67a
Blubberli <neele.witte@student.uni-tuebingen.de>
2020-02-10 10:08:31 +0100

    update tests and measures

--- germanetpy/graphbased_relatedness_measures.py
+++ germanetpy/graphbased_relatedness_measures.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import fastenum
 from germanetpy import longest_shortest_path
@@ -14,7 +15,7 @@ class GraphRelatedness:
     def __init__(self, germanet, category):
         self._germanet = germanet
         self._category = category
-        self._max_len, self._max_depth, synset_pair = longest_shortest_path.get_overall_longest_shortest_distance(
+        self._max_len, self._max_depth, synset_pair = longest_shortest_path.get_longest_possible_shortest_distance(
             germanet, category)
         self._normalization_dic = self.init_min_max_normalization_values(synset_pair)
 
@@ -48,10 +49,10 @@ class GraphRelatedness:
         }
         return norm_values
 
-    def wu_and_palmer(self, synset1, synset2, normalize=False, upper_bound=1.0):
+    def wu_and_palmer(self, synset1, synset2, normalize=False, normalized_max=1.0):
         assert synset1.word_category == synset2.word_category, "only synsets of the same Wordcategory can be " \
                                                                "compared"
-        root_node = self.germanet.root()
+        root_node = self.germanet.root
         lcs_nodes = synset1.lowest_common_subsumer(synset2)
         depth = 0
         for n in lcs_nodes:
@@ -61,22 +62,32 @@ class GraphRelatedness:
         pathlen = synset2.shortest_path_distance(synset1)
         wup = (2 * depth) / (pathlen + 2 * depth)
         if normalize:
-            wup = self.normalize(raw_value=wup, normalized_max=upper_bound, semrel_measure=SemRelMeasure.WuAndPalmer)
-        return np.round(wup, decimals=3)
+            wup = self.normalize(raw_value=wup, normalized_max=normalized_max, semrel_measure=SemRelMeasure.WuAndPalmer)
+        return np.round(wup, decimals=5)
 
-    def leacock_chodorow(self, synset1, synset2, normalize=False, upper_bound=1.0):
+    def leacock_chodorow(self, synset1, synset2, normalize=False, normalized_max=1.0):
+        """
+        This method implements the leackock and chodorow similarity measure. For the path distance and depth, node count
+        is used.
+        :param synset1: The source synset
+        :param synset2: The target synset the source synset is compared to
+        :param normalize: The relatedness value can be normalized to a number between the possible minimum of that
+        measure and a given upper bound.
+        :param upper_bound: [float] The upper bound of the range the measure is normalized to. The lower bound is
+        :return:
+        """
         assert synset1.word_category == synset2.word_category, "only synsets of the same Wordcategory can be " \
                                                                "compared"
-        pathlen = synset1.shortest_path_distance(synset2)
-        lch_sim = -np.log(pathlen / (2 * self.max_depth))
+        pathlen = synset1.shortest_path_distance(synset2) + 1
+        lch_sim = -math.log10(pathlen / (2 * (self.max_depth + 1)))
         if normalize:
-            lch_sim = self.normalize(raw_value=lch_sim, normalized_max=upper_bound,
+            lch_sim = self.normalize(raw_value=lch_sim, normalized_max=normalized_max,
                                      semrel_measure=SemRelMeasure.LeacockAndChodorow)
-        return np.round(lch_sim, decimals=3)
+        return np.round(lch_sim, decimals=5)
 
     def normalize(self, raw_value, normalized_max, semrel_measure):
         lower_bound, upper_bound = self.normalization_dic[semrel_measure]
-        return (raw_value / upper_bound) * normalized_max
+        return np.round((raw_value / upper_bound) * normalized_max, decimals=5)
 
     @property
     def germanet(self):
@@ -97,3 +108,23 @@ class GraphRelatedness:
     @property
     def normalization_dic(self):
         return self._normalization_dic
+
+
+if __name__ == '__main__':
+    from germanetpy.germanet import Germanet
+    from germanetpy.synset import WordCategory
+
+    germanet_data = Germanet("/Users/nwitte/PycharmProjects/germanetpy/data")
+    rel = GraphRelatedness(germanet_data, WordCategory.verben)
+
+    # relatedness_nouns = GraphRelatedness(germanet=germanet_data, category=WordCategory.nomen)
+    s1 = germanet_data.get_synset_by_id("s57534")
+    s2 = germanet_data.get_synset_by_id("s119463")
+    print(s1)
+    print(s2)
+    # print(rel.normalization_dic)
+    print("raw")
+    print(rel.leacock_chodorow(s1, s2))
+    print("normalized")
+    print(rel.leacock_chodorow(s1, s2, normalize=True))
+    print(s1.shortest_path_distance(s2))
