fb0107012a8c1634e971b60fea324854fd092da0
bencampbell30 <bcmpbell@bens-imac.kep2.uni-tuebingen.de>
2025-09-17 10:47:48 +0200

    Updating version number

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requirements = ["numpy>=1.18.1", "lxml>=4.4.2", "pytest>=5.3.2", "fastenum>=0.0.
 long_description = (HERE / "README.md").read_text()
 setuptools.setup(
     name="germanetpy",
-    version="0.2.3",
+    version="0.2.4",
     author="Ben Campbell",
     author_email="ben.campbell@uni-tuebingen.de",
     description="Python API for GermaNet",
